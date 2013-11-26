af5fd1b3fc4cb7594268277620ed24f1c274afd7
Alex Gilleran <alex@alexgilleran.com>
2013-11-26 15:23:41 +1100

    Fixed exercise view dropdown selection.

--- HIITMe/src/com/alexgilleran/hiitme/presentation/programdetail/views/ExerciseView.java
+++ HIITMe/src/com/alexgilleran/hiitme/presentation/programdetail/views/ExerciseView.java
@@ -2,10 +2,12 @@ package com.alexgilleran.hiitme.presentation.programdetail.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.alexgilleran.hiitme.R;
 import com.alexgilleran.hiitme.model.Exercise;
@@ -14,15 +16,18 @@ public class ExerciseView extends LinearLayout {
 
 	private Spinner effortLevel;
 	private EditText duration;
-
 	private Exercise exercise;
+
+	final LayoutInflater inflater;
 
 	public ExerciseView(Context context) {
 		super(context);
+		inflater = LayoutInflater.from(context);
 	}
 
 	public ExerciseView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		inflater = LayoutInflater.from(context);
 	}
 
 	@Override
@@ -32,15 +37,16 @@ public class ExerciseView extends LinearLayout {
 		effortLevel = (Spinner) findViewById(R.id.exercise_effort_level);
 		duration = (EditText) findViewById(R.id.exercise_duration);
 
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(),
-				android.R.layout.simple_spinner_item, new String[] { "Hard", "Easy", "Rest" });
-		arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-		effortLevel.setAdapter(arrayAdapter);
+		effortLevel.setAdapter(new EffortLevelAdapter());
 	}
 
 	public void setExercise(Exercise exercise) {
 		this.exercise = exercise;
+
+		render();
 	}
 
+	private void render() {
+		effortLevel.setSelection(exercise.getEffortLevel().ordinal());
+	}
 }
